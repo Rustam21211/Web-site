ce34591a6caf9cb64859a2aaa7fb8cc5a0d06808
Rustam21211 <132743256+Rustam21211@users.noreply.github.com>
2024-12-10 15:55:07 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll(".menu-item > a");
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function() {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
+    }
+}
+// alert(1111);
